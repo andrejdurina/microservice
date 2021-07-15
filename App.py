a848dfe1b8963338e9344118c6f11a36294eabfb
Andrej Ďurina <xdurinaa@stuba.sk>
2021-07-15 17:01:59 +0000

    Added Flask basic authorization.

--- App.py
+++ App.py
@@ -1,7 +1,18 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
+from flask.helpers import make_response
+from functools import wraps
 import psutil,ast,sys
 
 app = Flask(__name__)
+
+def auth_required(f):
+        @wraps(f)
+        def decorated(*args,**kwargs):
+                auth=request.authorization
+                if auth and auth.username == 'user' and auth.password == 'pass':
+                        return f(*args,**kwargs)
+                return make_response('Bad credentials',401,{'WWW-Authenticate' : 'Basic realm = "Login Required"'})
+        return decorated
 
 """"Creates dictionary from.txt with key values"""
 file = open("Dictionary.txt","r")
@@ -11,10 +22,11 @@ indicators = ast.literal_eval(contents)
 """Basic home routing"""
 @app.route("/",methods=["GET"])
 def home():
-  return "Welcome to HomePage!"
+  return "Welcome to testing page, to advance append /run to url!"
 
 """Routing /run displays JSON with parameters of CPU,RAM,Disk & Network usage of current container."""
 @app.route("/run", methods=["GET","POST"])
+@auth_required
 def get_updated_dict():
      if not indicators:
         return -1
@@ -27,10 +39,11 @@ def get_updated_dict():
             if (key == "Disk"):
                     indicators[key] = str(psutil.disk_usage('/').percent)
             if (key == "Network"):
-                    indicators[key]['recieved'] = str(psutil.net_io_counters().bytes_recv)
-                    indicators[key]['sent'] = str(psutil.net_io_counters().bytes_sent)
+                    indicators[key]['recieved'] = str(float(psutil.net_io_counters().bytes_recv)*10 ** -6)
+                    indicators[key]['sent'] = str(float(psutil.net_io_counters().bytes_sent)*10 ** -6) # In Megabytes
         print(indicators,sys.stdout)
      return jsonify(indicators)
  
 if __name__ == "__main__":
+  app.config['BASE_AUTH_FORCE'] = True
   app.run(debug="true")
